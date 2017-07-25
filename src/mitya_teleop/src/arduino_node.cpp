f38b81d54ab2f02b27564c4d4889ce21c667dd1c
DmitryDzz <DzakhovDS@gmail.com>
2017-07-25 23:30:56 +0200

    #5 Serial baud rate param

--- src/mitya_teleop/src/arduino_node.cpp
+++ src/mitya_teleop/src/arduino_node.cpp
@@ -53,11 +53,13 @@ public:
   void publish(char *message);
 private:
   std::string serialPortName;
+  int serialBaudRate;
   int fd;
   char serialOutcomingMessage[MAX_MESSAGE_SIZE];
   char serialIncomingMessage[MAX_MESSAGE_SIZE];
   int serialIncomingMessageSize;
   char buffer[SERIAL_BUFFER_SIZE];
+  int baudRateToBaudRateConst(int baudRate);
 
   ros::Subscriber arduinoInputSubscriber_;
   void arduinoInputCallback(const std_msgs::StringConstPtr& msg);
@@ -85,6 +87,17 @@ ArduinoNode::ArduinoNode()
   else
   {
     ROS_ERROR("Failed to get parameter '%s'", serialPortParamName.c_str());
+    serialPortName = "unassigned";
+  }
+  std::string baudRateParamName = "baud_rate";
+  if (privateNodeHandle.getParam(baudRateParamName, serialBaudRate))
+  {
+    ROS_INFO("Serial port baud rate: %d", serialBaudRate);
+  }
+  else
+  {
+    serialBaudRate = 9600;
+    ROS_ERROR("Failed to get parameter '%s'. The default value is %d.", baudRateParamName.c_str(), serialBaudRate);
   }
 
   fd = -1;
@@ -177,11 +190,81 @@ void ArduinoNode::openSerial()
     ROS_ERROR("Error %d opening %s: %s", errno, serialPortName.c_str(), strerror(errno));
     return;
   }
-  if (!setInterfaceAttribs(fd, B9600, 0)) // set speed to 9,600 bps, 8n1 (no parity)
+  if (!setInterfaceAttribs(fd, baudRateToBaudRateConst(serialBaudRate), 0)) // set speed bps, 8n1 (no parity)
     return;
-  if (!setBlocking(fd, 0))                // set no blocking
+  if (!setBlocking(fd, 0)) // set no blocking
     return;
   ROS_INFO("Serial port is opened");
+}
+
+int ArduinoNode::baudRateToBaudRateConst(int baudRate)
+{
+  switch (baudRate)
+  {
+    case 50:
+      return B50;
+    case 75:
+      return B75;
+    case 110:
+      return B110;
+    case 134:
+      return B134;
+    case 150:
+      return B150;
+    case 200:
+      return B200;
+    case 300:
+      return B300;
+    case 600:
+      return B600;
+    case 1200:
+      return B1200;
+    case 1800:
+      return B1800;
+    case 2400:
+      return B2400;
+    case 4800:
+      return B4800;
+    case 9600:
+      return B9600;
+    case 19200:
+      return B19200;
+    case 38400:
+      return B38400;
+    case 57600:
+      return B57600;
+    case 115200:
+      return B115200;
+    case 230400:
+      return B230400;
+    case 460800:
+      return B460800;
+    case 500000:
+      return B500000;
+    case 576000:
+      return B576000;
+    case 921600:
+      return B921600;
+    case 1000000:
+      return B1000000;
+    case 1152000:
+      return B1152000;
+    case 1500000:
+      return B1500000;
+    case 2000000:
+      return B2000000;
+    case 2500000:
+      return B2500000;
+    case 3000000:
+      return B3000000;
+    case 3500000:
+      return B3500000;
+    case 4000000:
+      return B4000000;
+    default:
+      ROS_ERROR("Wrong serial baud rate value. The default value is 9600.");
+      return B9600;
+  }
 }
 
 void ArduinoNode::readSerial(void (*func)(ArduinoNode*, char*))
