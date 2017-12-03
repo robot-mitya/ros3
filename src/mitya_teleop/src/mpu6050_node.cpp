535ef3b710139b47c7cf56a27f4e53563f6c954f
DmitryDzz <DzakhovDS@gmail.com>
2017-12-03 18:05:31 +0300

    #12 mpu6050_node on PC (temporarily)

---
+++ src/mitya_teleop/src/mpu6050_node.cpp
@@ -0,0 +1,94 @@
+/*
+ * mpu6050_node.cpp
+ * Copyright (c) 2017, Robot Mitya.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Robot Mitya nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  Created on: Dec 3, 2017
+ *      Author: Dmitry Dzakhov
+ */
+#include <ros/ros.h>
+#include <sensor_msgs/Imu.h>
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+
+using namespace std;
+
+const int I2C_ADDR = 0x68;
+const int PWR_MGMT_1 = 0x6B;
+
+float read_word_2c(int fd, int addr) {
+  int high = wiringPiI2CReadReg8(fd, addr);
+  int low = wiringPiI2CReadReg8(fd, addr+1);
+  int val = (high << 8) + low;
+  return float((val >= 0x8000) ? -((65535 - val) + 1) : val);
+}
+
+int main(int argc, char **argv) {
+
+  // Connect to device.
+  int fd = wiringPiI2CSetup(I2C_ADDR);
+  if (fd == -1) {
+    printf("No i2c device found?\n");
+    return -1;
+  }
+  // Device starts in sleep mode so wake it up.
+  wiringPiI2CWriteReg16(fd, PWR_MGMT_1, 0);
+
+  // Start ROS node stuff.
+  ros::init(argc, argv, "mpu6050");
+  ros::NodeHandle node;
+  ros::Publisher pub = node.advertise<sensor_msgs::Imu>("imu", 10);
+  ros::Rate rate(10);  // hz
+
+  // Publish in loop.
+  while(ros::ok()) {
+    sensor_msgs::Imu msg;
+    msg.header.stamp = ros::Time::now();
+    msg.header.frame_id = '0';  // no frame
+
+    // Read gyroscope values.
+    // At default sensitivity of 250deg/s we need to scale by 131.
+    msg.angular_velocity.x = read_word_2c(fd, 0x43) / 131;
+    msg.angular_velocity.y = read_word_2c(fd, 0x45) / 131;
+    msg.angular_velocity.z = read_word_2c(fd, 0x47) / 131;
+
+    // Read accelerometer values.
+    // At default sensitivity of 2g we need to scale by 16384.
+    // Note: at "level" x = y = 0 but z = 1 (i.e. gravity)
+    // But! Imu msg docs say acceleration should be in m/2 so need to *9.807
+    const float la_rescale = 16384.0 / 9.807;
+    msg.linear_acceleration.x = read_word_2c(fd, 0x3b) / la_rescale;
+    msg.linear_acceleration.y = read_word_2c(fd, 0x3d) / la_rescale;
+    msg.linear_acceleration.z = read_word_2c(fd, 0x3f) / la_rescale;
+
+    // Pub & sleep.
+    pub.publish(msg);
+    ros::spinOnce();
+    rate.sleep();
+  }
+  return 0;
+}
