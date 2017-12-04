499bbda72c0c53a3fb7f5519798908f9659236d0
DmitryDzz <DzakhovDS@gmail.com>
2017-12-04 19:49:18 +0300

    #12 mpu6050_node refactoring

--- src/mitya_teleop/src/mpu6050_node.cpp
+++ src/mitya_teleop/src/mpu6050_node.cpp
@@ -36,58 +36,82 @@
 #include <wiringPiI2C.h>
 #include "consts.h"
 
-using namespace std;
+class Mpu6050Node
+{
+public:
+  Mpu6050Node();
+  void publishImu();
+private:
+  int fileDescriptor_;
+  float readWord2c(int addr);
+
+  // Topic RM_IMU_TOPIC_NAME ('imu') publisher:
+  ros::Publisher imuPublisher_;
+};
 
 const int I2C_ADDR = 0x68;
 const int PWR_MGMT_1 = 0x6B;
 
-float read_word_2c(int fd, int addr) {
-  int high = wiringPiI2CReadReg8(fd, addr);
-  int low = wiringPiI2CReadReg8(fd, addr+1);
+Mpu6050Node::Mpu6050Node()
+{
+  // Connect to device.
+  fileDescriptor_ = wiringPiI2CSetup(I2C_ADDR);
+  if (fileDescriptor_ == -1)
+  {
+    ROS_ERROR("No i2c device found?");
+    return;
+  }
+  // Device starts in sleep mode so wake it up.
+  wiringPiI2CWriteReg16(fileDescriptor_, PWR_MGMT_1, 0);
+
+  ros::NodeHandle nodeHandle(RM_NAMESPACE);
+  imuPublisher_ = nodeHandle.advertise<sensor_msgs::Imu>(RM_IMU_TOPIC_NAME, 100);
+}
+
+float Mpu6050Node::readWord2c(int addr)
+{
+  int high = wiringPiI2CReadReg8(fileDescriptor_, addr);
+  int low = wiringPiI2CReadReg8(fileDescriptor_, addr + 1);
   int val = (high << 8) + low;
   return float((val >= 0x8000) ? -((65535 - val) + 1) : val);
 }
 
-int main(int argc, char **argv) {
+void Mpu6050Node::publishImu()
+{
+  sensor_msgs::Imu msg;
+  msg.header.stamp = ros::Time::now();
+  msg.header.frame_id = '0';  // no frame
 
-  // Connect to device.
-  int fd = wiringPiI2CSetup(I2C_ADDR);
-  if (fd == -1) {
-    printf("No i2c device found?\n");
-    return -1;
-  }
-  // Device starts in sleep mode so wake it up.
-  wiringPiI2CWriteReg16(fd, PWR_MGMT_1, 0);
+  // Read gyroscope values.
+  // At default sensitivity of 250deg/s we need to scale by 131.
+  msg.angular_velocity.x = readWord2c(0x43) / 131;
+  msg.angular_velocity.y = readWord2c(0x45) / 131;
+  msg.angular_velocity.z = readWord2c(0x47) / 131;
 
-  // Start ROS node stuff.
+  // Read accelerometer values.
+  // At default sensitivity of 2g we need to scale by 16384.
+  // Note: at "level" x = y = 0 but z = 1 (i.e. gravity)
+  // But! Imu message documentations say acceleration should be in m/2 so need to *9.807
+  const float la_rescale = 16384.0 / 9.807;
+  msg.linear_acceleration.x = readWord2c(0x3b) / la_rescale;
+  msg.linear_acceleration.y = readWord2c(0x3d) / la_rescale;
+  msg.linear_acceleration.z = readWord2c(0x3f) / la_rescale;
+
+  // Pub & sleep.
+  imuPublisher_.publish(msg);
+}
+
+int main(int argc, char **argv)
+{
   ros::init(argc, argv, "mpu6050");
-  ros::NodeHandle node;
-  ros::Publisher pub = node.advertise<sensor_msgs::Imu>(RM_IMU_TOPIC_NAME, 10);
-  ros::Rate rate(10);  // hz
 
+  Mpu6050Node mpu6050Node;
+
+  ros::Rate rate(10);  // hz
   // Publish in loop.
   while(ros::ok()) {
-    sensor_msgs::Imu msg;
-    msg.header.stamp = ros::Time::now();
-    msg.header.frame_id = '0';  // no frame
+    mpu6050Node.publishImu();
 
-    // Read gyroscope values.
-    // At default sensitivity of 250deg/s we need to scale by 131.
-    msg.angular_velocity.x = read_word_2c(fd, 0x43) / 131;
-    msg.angular_velocity.y = read_word_2c(fd, 0x45) / 131;
-    msg.angular_velocity.z = read_word_2c(fd, 0x47) / 131;
-
-    // Read accelerometer values.
-    // At default sensitivity of 2g we need to scale by 16384.
-    // Note: at "level" x = y = 0 but z = 1 (i.e. gravity)
-    // But! Imu msg docs say acceleration should be in m/2 so need to *9.807
-    const float la_rescale = 16384.0 / 9.807;
-    msg.linear_acceleration.x = read_word_2c(fd, 0x3b) / la_rescale;
-    msg.linear_acceleration.y = read_word_2c(fd, 0x3d) / la_rescale;
-    msg.linear_acceleration.z = read_word_2c(fd, 0x3f) / la_rescale;
-
-    // Pub & sleep.
-    pub.publish(msg);
     ros::spinOnce();
     rate.sleep();
   }
