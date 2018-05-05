57e12559a5155783712ae448b440f8dcfa5eb5ec
DmitryDzz <DzakhovDS@robot-mitya.ru>
2018-05-05 22:14:48 +0300

    #30 VPN adaptation: env.bash

--- env.bash
+++ env.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 #source /opt/ros/kinetic/setup.bash
-export ROS_IP=192.168.100.235
-export ROS_MASTER_URI=http://192.168.100.3:11311/
+#export ROS_IP=192.168.100.235
+#export ROS_MASTER_URI=http://192.168.100.3:11311/
+export ROS_IP=10.8.0.7
+export ROS_MASTER_URI=http://10.8.0.2:11311/
 
 source /home/dmitrydzz/dev/mitya3/ros3/devel/setup.bash
 
